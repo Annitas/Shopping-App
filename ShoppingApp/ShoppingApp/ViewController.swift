67896e828eb083a355956764c785d4ced07c3c03
Annitas <39757257+Annitas@users.noreply.github.com>
2023-08-27 16:45:40 +0600

    Delete Storyboard

---
+++ ShoppingApp/ShoppingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Anita Stashevskaya on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightText
+        
+    }
+
+
+}
+
